402a370dab1cdbc1af43e201d8ab7b87b577af46
ElifKartal <77572344+ElifKartal@users.noreply.github.com>
2021-01-23 23:55:25 +0300

    Add files via upload

---
+++ OOP2/GercekMusteri.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP2
+{
+    //ındıvıdual
+    class GercekMusteri:Musteri
+    {
+        public string TcNo { get; set; }
+        public string Adi { get; set; }
+        public string Soyadi { get; set; }
+       
+    }
+}
