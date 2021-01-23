05ba237519c1c11953fda5f7fca4eb0f735a949c
ElifKartal <77572344+ElifKartal@users.noreply.github.com>
2021-01-23 23:56:47 +0300

    Add files via upload

---
+++ OOP3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OOP3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IKrediManager ihtiyacKrediManager = new IhtiyacKrediManager();
+            IKrediManager konutKrediManager = new KonutKrediManager();
+            IKrediManager tasitKrediManager = new TasitKrediManager();
+           
+            
+            
+            ILoggerService databaseLogerService = new DatabaseLoggerService();
+            ILoggerService fileLogerService = new FileLoggerService();
+            
+            
+            
+            BasvuruManager basvuruManager = new BasvuruManager();
+            basvuruManager.BasvuruYap(tasitKrediManager,databaseLogerService);
+
+            List<IKrediManager> krediler = new List<IKrediManager>() {ihtiyacKrediManager,tasitKrediManager };
+
+            //basvuruManager.KrediOnBilgilendirmesiYap(krediler);
+
+        }
+    }
+}
